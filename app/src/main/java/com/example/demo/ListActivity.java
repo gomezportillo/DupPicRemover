78c888478b0991f25acdb6c6f9bd995fea3503dc
Pedro Manuel Gómez-Portillo <pedroma.almagro@gmail.com>
2019-05-04 20:30:34 +0200

    Passing arguments to second activity and filling listview with test data

--- app/src/main/java/com/example/demo/ListActivity.java
+++ app/src/main/java/com/example/demo/ListActivity.java
@@ -1,16 +1,127 @@
 package com.example.demo;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class ListActivity extends AppCompatActivity {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class ListActivity extends AppCompatActivity
+{
+    private ListView lv_files;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-        getSupportActionBar().setTitle("Pene");
+        getSupportActionBar().setTitle(R.string.dup_images_txt);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Get parameters from prev. activity
+        // REF: https://stackoverflow.com/a/5265952/3594238
+        boolean recursive;
+        String path;
+        if (savedInstanceState == null)
+        {
+            Bundle extras = getIntent().getExtras();
+            recursive = extras.getBoolean("Recursive");
+            path = extras.getString("Path");
+        }
+        else
+        {
+            recursive = (boolean) savedInstanceState.getSerializable("Recursive");
+            path = (String) savedInstanceState.getSerializable("Path");
+
+        }
+//        Toast.makeText(getApplicationContext(), "Path " + path , Toast.LENGTH_SHORT).show();
+
+        // Search for files
+        List<String> files = null;
+
+        File picture_file = new File(path);
+        if (picture_file.exists())
+        {
+            Toast.makeText(getApplicationContext(), "Path _" + path + "_", Toast.LENGTH_SHORT).show();
+
+
+            if (recursive)
+            {
+                files = getListFilesRecursive(picture_file);
+            }
+            else
+            {
+                files = getListFiles(picture_file);
+            }
+            Toast.makeText(getApplicationContext(), "List view size: " + files.size(), Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(), "Path _" + path + "_ does not exists", Toast.LENGTH_SHORT).show();
+
+            files = new ArrayList<String>();
+            files.add("Hola");
+            files.add("Hola");
+            files.add("Hola");
+        }
+
+        // REF. https://stackoverflow.com/a/5070922/3594238
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                files);
+
+        lv_files = findViewById(R.id.listview_files);
+        lv_files.setAdapter(arrayAdapter);
+    }
+
+
+    //REF. https://stackoverflow.com/a/9531063/3594238
+    private List<String> getListFilesRecursive(File parentDir)
+    {
+        ArrayList<String> inFiles = new ArrayList<String>();
+        File[] files = parentDir.listFiles();
+        for (File file : files)
+        {
+            if (file.isDirectory())
+            {
+                inFiles.addAll(getListFilesRecursive(file));
+            }
+            else
+            {
+                inFiles.add(file.getName());
+            }
+        }
+        return inFiles;
+    }
+
+    //REF. https://stackoverflow.com/a/9531063/3594238
+    private List<String> getListFiles(File parentDir)
+    {
+        List<String> inFiles = new ArrayList<>();
+        Queue<File> files = new LinkedList<>();
+        files.addAll(Arrays.asList(parentDir.listFiles()));
+        while (!files.isEmpty())
+        {
+            File file = files.remove();
+            if (file.isDirectory())
+            {
+                files.addAll(Arrays.asList(file.listFiles()));
+            }
+            else
+            {
+                inFiles.add(file.getName());
+            }
+        }
+        return inFiles;
     }
 }
--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -1,25 +1,46 @@
 package com.example.demo;
 
 import android.content.Intent;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    int REQUEST_DIRECTORY = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn =  findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
+        // Set the download path as default path
+        String download_path = Environment.DIRECTORY_DOWNLOADS;
+        EditText text_path = findViewById(R.id.text_path);
+        text_path.setText(download_path);
 
+        text_path.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+             // Buscador de directorios
+            }
+        });
+
+        findViewById(R.id.button_search).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                EditText text_path = findViewById(R.id.text_path);
+                Switch switch_recursive = findViewById(R.id.switch_recursive);
+
                 Intent intent = new Intent(MainActivity.this, ListActivity.class);
-                //intent.putExtra("Historia", historia);
+
+                intent.putExtra("Path", text_path.getText().toString());
+                intent.putExtra("Recursive", switch_recursive.isChecked());
+
                 startActivityForResult(intent, 0);
             }
         });
